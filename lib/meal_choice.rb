b138b99d1cb90081ca5d5f5c87428ad4ff3e9d35
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-17 19:50:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 
-def meal_choice(opt1, opt2, meat = "tofu")
+def meal_choice(side1, side2, meat = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{meat} with #{opt1} and #{opt2}."
 end
