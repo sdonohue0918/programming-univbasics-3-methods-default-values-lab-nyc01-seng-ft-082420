5614c75c2aa440fe005dbdd5e7efcf4d953b5cd4
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-17 22:50:13 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,16 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+
+def meal_choice(side1, side2, protein = "tofu")
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{side1} and #{side2}."
+  puts meal
+
+  return meal
+end
+
+
+meal_choice("broccoli", "macaroni")
+
+meal_choice("broccoli", "macaroni", "roast beef")
