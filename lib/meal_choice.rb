531058c2b0bd54cbaed554b15e3ccc050a8c21a1
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-17 22:48:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,8 @@
 
 def meal_choice(side1, side2, protein = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{side1} and #{side2}."
+  meal = "A plate of #{protein} with #{side1} and #{side2}."
+  return meal
 end
 
 
